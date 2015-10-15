cdf7cbad3cf6c0de360a1ea5c6eab7abb4ae4d37
michpenn <michpenn@icloud.com>
2015-10-15 10:27:12 -0700

    memory_match v0.5 - Michal Penn

---
+++ memory_game.js
@@ -0,0 +1,11 @@
+/**
+ * Created by michpenn on 10/14/15.
+ */
+var first_card_clicked = null;
+var second_card_clicked = null;
+var total_possible_matches = 2;
+var match_counter = 0;
+
+function card_clicked() {
+
+}
