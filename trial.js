e52fa20d1b8a44dee7138ac6a4c333a2ef51f148
michpenn <michpenn@icloud.com>
2015-11-21 19:42:19 -0800

    level selection and preferences work, now need to integrate OOP

--- trial.js
+++ trial.js
@@ -2,11 +2,15 @@
  * Created by michpenn on 11/21/15.
  */
 var gameTheme;
+var gameLevel;
+var iPhone;
+var iPad;
 
-
+//setting preferences before generating the game board
+//STEP 1
 function selectTheme() {
     var theme_container = $('<div>', {
-        class: 'preference_container container-fluid'
+        class: 'theme_container container'
     });
     var theme_heading = $('<h3>', {
         class: 'preference_heading text-center',
@@ -14,7 +18,7 @@ function selectTheme() {
     });
 
     var button_container = $('<div>', {
-        class: 'row'
+        class: 'row row_buttons_theme'
     });
 
     var disney = $('<button>', {
@@ -25,7 +29,6 @@ function selectTheme() {
     });
 
 
-
     var candidates = $('<button>', {
         class: 'col-xs-5 col-xs-offset-2 col-sm-4 col-sm-offset-2 btn btn-primary btn-lg theme_button candidates_button',
         type: 'button',
@@ -34,16 +37,108 @@ function selectTheme() {
     });
 
 
-
     $(button_container).append(disney, candidates);
     $(theme_container).append(theme_heading, button_container);
     $('#preferences_container').append(theme_container);
-    $('.theme_button').click(function() {
+    $('.theme_button').click(function () {
         gameTheme = ($(this).attr('theme'));
+        if(gameTheme == 'disney'){
+            if(iPhone == true) {
+                $('body').addClass('disney_phone_background');
+            }
+            else if (iPad == true) {
+                $('body').addClass('disney_ipad_background');
+            }
+
+            else{
+                $('body').addClass('disney_background');
+            }
+        }
+        else if(gameTheme == 'candidates'){
+            console.log('set candidate background');
+            if(iPhone == true) {
+                $('body').addClass('candidate_iphone_background');
+            }
+            else if (iPad == true) {
+                $('body').addClass('candidate_ipad_background');
+            }
+            else{
+                $('body').addClass('candidate_background');
+            }
+        }
+        $('.theme_container').addClass('removeThemes');
+        setTimeout(selectLevel, 3000);
+
     })
 }
+//STEP2
+function selectLevel() {
+    $('.theme_container').remove();
+    $('.welcome').remove();
+    var level_container = $('<div>', {
+        class: 'level_container container enterLevels'
+    });
+    var level_heading = $('<h3>', {
+        class: 'preference_heading text-center',
+        text: 'Please select a level'
+    });
+
+    var button_container = $('<div>', {
+        class: 'row row_buttons row_buttons_level'
+    });
+
+    var easy = $('<button>', {
+        class: 'btn btn-primary outline level_button easy_button col-sm-offset-1',
+        type: 'button',
+        level: 'easy',
+        text: 'Easy'
+    });
+
+    var medium = $('<button>', {
+        class: 'btn btn-primary outline level_button medium_button col-sm-offset-2',
+        type: 'button',
+        level: 'medium',
+        text: 'Medium'
+    });
+
+    var hard = $('<button>', {
+        class: 'btn btn-primary outline level_button hard_button col-sm-offset-2',
+        type: 'button',
+        level: 'hard',
+        text: 'Hard'
+    });
+
+    $(button_container).append(easy, medium,hard);
+    $(level_container).append(level_heading, button_container);
+    $('#preferences_container').append(level_container);
+    $('.level_button').click(function () {
+        gameLevel = ($(this).attr('level'));
+        console.log(gameLevel);
+        //TODO clear level selection and load game board and card
+        //TODO also make a warning about the different versions
+    });
+}
+
+//Step 3: check device type
+function checkDevise() {
+    if(navigator.userAgent.search("iPhone") >= 0) {
+        console.log('version iPhone');
+        iPhone = true;
+    }
+    if(navigator.userAgent.search("iPad") >= 0) {
+        console.log('version iPad');
+        iPad = true;
+    }
+    else {
+        console.log(navigator.userAgent);
+    }
+}
+
+//Step 4: generate game board
+
 
 $(document).ready(function () {
     console.log('ready to load');
+    checkDevise();
     selectTheme();
 });
